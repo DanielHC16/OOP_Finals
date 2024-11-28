97da152f833b38c77bef4c74dc2148f697bf39f5
Daniel Camacho <danielcamacho0416@gmail.com>
2024-11-29 00:55:05 +0800

    Switched to MySQL Workbench

--- src/finalsoop/finalsConnect.java
+++ src/finalsoop/finalsConnect.java
@@ -1,23 +1,29 @@
 package finalsoop;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class finalsConnect {
-    
-    public static Connection Connect(){
-        
+
+    public static Connection Connect() {
+
         Connection conn = null;
-        
+
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "finals_oop", "password"); // 
-            System.out.println("Connection Succesful");
-        } catch (Exception e){
-            System.out.println(e);
+            // Load the MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // JDBC URL
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/finalsoop?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "password");
+            
+            System.out.println("Connection Successful");
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e.getMessage());
+            e.printStackTrace();
         }
-        
+
         return conn;
-        
+
     }
-    
-    
+
 }
